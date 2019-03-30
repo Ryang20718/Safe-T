5aafea6dd12d0cccedc186d99eb780a88099d879
Ryan Yang <Ryanyang99@hotmail.com>
2019-03-30 16:26:09 -0700

    update

--- predict.py
+++ predict.py
@@ -11,10 +11,10 @@ from google.cloud.automl_v1beta1.proto import service_pb2
 
 # project id cs131-1552025416924 
 # model id ICN6390988586782325408
-def get_prediction(content, project_id, model_id):
+def get_prediction(content):
   prediction_client = automl_v1beta1.PredictionServiceClient()
 
-  name = 'projects/{}/locations/us-central1/models/{}'.format(project_id, model_id)
+  name = 'projects/{}/locations/us-central1/models/{}'.format("cs131-1552025416924" , "ICN6390988586782325408")
   payload = {'image': {'image_bytes': content }}
   params = {}
   request = prediction_client.predict(name, payload, params)
